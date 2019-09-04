b7e7c4c966efc0c04d823bf3ffe333ab12527683
Yaowenqiang <yaowenqiang111@163.com>
2019-09-05 03:16:14 +0800

    add swithc control demo

--- maps.go
+++ maps.go
@@ -21,4 +21,22 @@ func main() {
 	fmt.Println(map3["a"])
 	delete(map3, "a")
 	fmt.Println(map3)
+
+	switch a := 1; {
+	case a == 1:
+		fmt.Println("The integer was == 1")
+		fallthrough
+	case a == 2:
+		fmt.Println("The integer was == 2")
+	case a == 3:
+		fmt.Println("The integer was == 3")
+		fallthrough
+	case a == 4:
+		fmt.Println("The integer was == 4")
+	case a == 5:
+		fmt.Println("The integer was == 5")
+		fallthrough
+	default:
+		fmt.Println("default case")
+	}
 }
