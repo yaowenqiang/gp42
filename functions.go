491989dce6c79a59a1e30f11e648efb27e8367eb
Yaowenqiang <yaowenqiang111@163.com>
2019-09-13 23:48:21 +0800

    add functions demo

--- functions.go
+++ functions.go
@@ -5,15 +5,31 @@ import (
 )
 
 func main() {
-
+	testa(1, 2, 1.1)
 }
 
 func IndexRune(s string, r rune) int {
 	for i, c := range s {
 		if c == r {
 			return 1
+		} else {
+			return i
 		}
 	}
 
-	return
+	return 1
+}
+
+func FunctionName(a int32, b float32) (t1 int32, t2 float32) {
+	return a, b
+}
+
+func testa(a, b int, z float32) bool {
+	i := 1
+	fmt.Printf("i = %+v\n", i)
+	return true
+}
+
+func testb(a, b int, z float32) bool {
+	return true
 }
