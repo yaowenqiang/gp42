43cb28daaedc39f04d3574e361d20d742aa1c636
Yaowenqiang <yaowenqiang111@163.com>
2019-09-14 23:16:05 +0800

    add fixed arguments function demo

--- functions.go
+++ functions.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	testa(1, 2, 1.1)
+	testc(1, 2, 1.1, 4, 5, 6, 7, 8)
 }
 
 func IndexRune(s string, r rune) int {
@@ -31,5 +32,11 @@ func testa(a, b int, z float32) bool {
 }
 
 func testb(a, b int, z float32) bool {
+	return true
+}
+
+func testc(a, b int, z float32, values ...int) bool {
+	//可变参数
+	fmt.Printf("values	 = %+v\n", values)
 	return true
 }
