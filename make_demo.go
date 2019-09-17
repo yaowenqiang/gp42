d9e5761c90f80b73a319aa24c66df31f2689ee83
Yaowenqiang <yaowenqiang111@163.com>
2019-09-18 03:29:07 +0800

    add const demo

--- make_demo.go
+++ make_demo.go
@@ -33,4 +33,19 @@ func main() {
 	fmt.Printf("new(S) = %+v\n", new(S))
 	fmt.Printf("sss = %+v\n", sss)
 
+	const (
+		c1 = imag(2i)
+		c2 = len([10]float64{2})
+		c3 = len([10]float64{c1})
+		c4 = len([10]float64{imag(2i)})
+		//c5 = len([1]float64{imag(z)})
+	)
+
+	fmt.Printf("c1 = %+v\n", c1)
+	fmt.Printf("c2 = %+v\n", c2)
+	fmt.Printf("c3 = %+v\n", c3)
+	fmt.Printf("c4 = %+v\n", c4)
+	var z complex128
+	fmt.Printf("z = %+v\n", z)
+
 }
