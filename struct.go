93d528a3674373de0b536fe704cde467c509a110
Yaowenqiang <yaowenqiang111@163.com>
2019-09-23 01:13:20 +0800

    add struct demo

---
+++ struct.go
@@ -0,0 +1,19 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+)
+
+type Student struct {
+	name string "学生名字"
+	Age  int    "学生年龄"
+	Room int    `json:"Roomid"`
+}
+
+func main() {
+	st := Student{"Titan", 14, 102}
+	fmt.Println(reflect.typeOf(st).Field(0).Tag)
+	fmt.Println(reflect.typeOf(st).Field(1).Tag)
+	fmt.Println(reflect.typeOf(st).Field(2).Tag)
+}
