6a7bc7a5b0d39e63891bd12e25b62b79c97389d6
Yaowenqiang <yaowenqiang111@163.com>
2019-09-24 02:47:59 +0800

    add embbed struct demo

--- struct.go
+++ struct.go
@@ -11,9 +11,19 @@ type Student struct {
 	Room int    `json:"Roomid"`
 }
 
+type Human struct {
+	name string
+}
+
+type Student1 struct {
+	Human
+	int
+}
+
 func main() {
 	st := Student{"Titan", 14, 102}
 	fmt.Println(reflect.typeOf(st).Field(0).Tag)
 	fmt.Println(reflect.typeOf(st).Field(1).Tag)
 	fmt.Println(reflect.typeOf(st).Field(2).Tag)
+
 }
