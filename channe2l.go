5651389305ab68f38abb0673d67111f31c413487
Yaowenqiang <yaowenqiang111@163.com>
2019-10-04 09:45:02 +0800

    add channel demo

---
+++ channe2l.go
@@ -0,0 +1,31 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan int, 10)
+	go send(c)
+	go recv(c)
+	time.Sleep(3 * time.Second)
+
+	close(c)
+}
+
+func send(c chan<- int) {
+	for i := 0; i < 10; i++ {
+		fmt.Println("send readey ", i)
+		c <- i
+		fmt.Println("send ", i)
+	}
+}
+
+func recv(c <-chan int) {
+	for i := range c {
+		fmt.Println("received ", i)
+
+	}
+}
